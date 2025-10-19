e5ffd25d9b04ac2377f0842afceeff0d34802f6a
p4r7h <parthos250@gmail.com>
2025-10-20 00:11:17 +0530

    added about

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.js";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
     return (<main>
-       <Navbar />
+       <Navbar/>
         <Hero/>
         <Cocktails/>
+        <About/>
     </main>)
 }
 
